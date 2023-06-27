9062afe2557e5c3fd119c18f7431110b95f5d307
Ana Paula <anapaulacoqueirodogado@gmail.com>
2023-06-27 07:53:22 -0300

    adicionei arquivos htm e js

---
+++ login.js
@@ -0,0 +1,27 @@
+//333333333333333333333333333333333333
+
+
+
+function verificar (){
+let email = document.getElementById('loginEmail').value;
+let senha = document.getElementById('loginSenha').value;
+if(!email || !senha){
+       alert("campos de preechimento obrigatório. Favor preencher");
+}else {
+
+       alert("Campos preenchidos com sucesso");
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+//vejq´feajjviefj9uf98jve9ije98cjehvjeohvçqedjq0efu9vjf
+//rqikfqerfuq89ruif89hbsaçiedaefjvfnvak 3rreivjre~jicijv
